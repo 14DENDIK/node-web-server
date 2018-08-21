bdeafda11c38c6f96e59daa6bf3e49e0a2053557
Sardor <sardor.mukhitdinov98@gmail.com>
2018-08-21 23:07:10 +0500

    Added new view <projects>

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Portfolio',
+		paragraphText: 'My Portfolio here'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Cannot fulfil the request.'
